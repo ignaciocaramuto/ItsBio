136251059d3f8c3b1f6cc489c9df15b649398001
Nacho <ignaciocaramuto@gmail.com>
2022-03-11 18:48:04 -0300

    Feat. Nuevo componente header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  items: MenuItem[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.items = [
+      {
+          label: 'Productos',
+          icon: 'fa fa-shopping-bag'
+      },
+      {
+          label: 'Quiénes somos',
+          icon: 'fa fa-info'
+      }
+    ];
+  }
+
+}
